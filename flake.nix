6c0d0bd9532d14ce071f9eeba3ca2987dfd432b9
hashwarlock <hashwarlock@protonmail.com>
2023-05-11 01:41:58 -0500

    Update inputs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,12 @@
   description = "Welcome to hashwarlock's dev website";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixos-generators.url = "github:nix-community/nixos-generators";
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils = { url = "github:numtide/flake-utils"; };
   };
-  outputs = { self, nixpkgs, nixos-generators, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nixos-generators, flake-utils, ... }@inputs:
     let
       flakeContext = {
         inherit inputs;
